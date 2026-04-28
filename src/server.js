6541663d4b5ece7e14ba0df1df9d8ddebe6e67ae
Noah Tunis <noahetunis@gmail.com>
2026-04-28 18:38:36 -0400

    Fix SVG MIME type: serve favicon.svg as image/svg+xml not text/plain

--- src/server.js
+++ src/server.js
@@ -37,6 +37,9 @@ function sendFile(response, filePath) {
     ".html": "text/html; charset=utf-8",
     ".js": "application/javascript; charset=utf-8",
     ".json": "application/json; charset=utf-8",
+    ".svg": "image/svg+xml",
+    ".png": "image/png",
+    ".ico": "image/x-icon",
   };
 
   if (!fs.existsSync(filePath)) {
